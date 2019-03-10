6f6149093aacf549e63a2c8a543668bd8cd3809b
patsapol <patsapol.pro@gmail.com>
2019-03-10 13:44:56 +0700

    add setActors in Movie model, Use MovieResponse data from fetchAll check text query with regular expression. If MovieResponse data match, create Movie data that is using title and Cast from MovieResponse data and push Movie data into list then return list

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,7 +1,12 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +24,19 @@ public class SimpleMovieSearchService implements MovieSearchService {
 		//TODO: Step 2 => Implement this method by using data from MovieDataService
 		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
 		// Please do not change @Component annotation on this class
-		return null;
+		MoviesResponse moviesResponse = movieDataService.fetchAll();
+		List<Movie> movieResult = new ArrayList<>();
+		for (MovieData movieData : moviesResponse) {
+			queryText = queryText.replaceAll("\\s","\\S");
+			Pattern pattern = Pattern.compile("(?i)\\b("+ queryText +")\\b");
+			Matcher matcher = pattern.matcher(movieData.getTitle());
+
+			if(matcher.find()) {
+				Movie movie = new Movie(movieData.getTitle());
+				movie.setActors(movieData.getCast());
+				movieResult.add(movie);
+			}
+		}
+		return movieResult;
 	}
 }
